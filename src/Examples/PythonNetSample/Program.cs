ac86d0f35f383859628324c8cdd2a10b68032d29
Dmitry Kolchev <dmitrykolchev@msn.com>
2024-06-20 19:47:17 +0300

    exmple prompts

--- src/Examples/PythonNetSample/Program.cs
+++ src/Examples/PythonNetSample/Program.cs
@@ -1,11 +1,12 @@
 ﻿using Python.Runtime;
-using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
+using System.Reflection.Metadata;
 
 namespace PythonNetSample;
 
 internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
         string PythonDll = @"C:\Program Files\Python310\python310.dll";
         Runtime.PythonDLL = PythonDll;
@@ -42,10 +43,15 @@ internal class Program
         var createPrompt = module.GetAttr("createPrompt");
         var prompt = createPrompt.Invoke(
             pipeline,
-            "score_9, score_8_up, score_7_up, Expressiveh, one sexy dark-skinned Mexican girl, long wet hair, female focus, lustrous skin, parted lips, slender, large breasts, through breasts silhouette, blurry body silhouette, naked transparent shirt, oversized shirt, shirt overhang, see-through shirt, see-through silhouette, HD32k, expressiveh, arched back, sexy pose, under an outdoor shower, wet skin, wet hair, perfect body, wide hips, narrow waist, barefoot, bottomless, perfect innie pussy, tight thin labia, hands on own head, wet hair, wet skin, wet shirt, supermodel pose, chest thrust out, outdoors, on the beach, sun high in the sky, well lit, sexy trimmed pubic hair, standing, showering, eyes closed, 3/4 angle view, three quarter view, view from below, (head titled up:1.4), (outdoor shower), arched back, contrapposto pose, (3/4 view:1.5),  (wet hair:1.4)",
-            "score_6, score_5, score_4, pony, gaping, muscular, censored, furry, milf, child, kid, chibi, monochrome, grayscale, bra, panties, upturned eyes"
+            //"score_9, score_8_up, score_7_up, Expressiveh, one sexy dark-skinned Mexican girl, long wet hair, female focus, lustrous skin, parted lips, slender, large breasts, through breasts silhouette, blurry body silhouette, naked transparent shirt, oversized shirt, shirt overhang, see-through shirt, see-through silhouette, HD32k, expressiveh, arched back, sexy pose, under an outdoor shower, wet skin, wet hair, perfect body, wide hips, narrow waist, barefoot, bottomless, perfect innie pussy, tight thin labia, hands on own head, wet hair, wet skin, wet shirt, supermodel pose, chest thrust out, outdoors, on the beach, sun high in the sky, well lit, sexy trimmed pubic hair, standing, showering, eyes closed, 3/4 angle view, three quarter view, view from below, (head titled up:1.4), (outdoor shower), arched back, contrapposto pose, (3/4 view:1.5),  (wet hair:1.4)",
+            //"score_9, score_8_up, score_7_up,  1girl, valkyrie in gorgeous armor is holding a spear in hand, (side view, solo:1.1), standing, golden laurel wreath crown, goddess, pale skin, beautiful face, armored dress, wings, looking at viewer, cloudy sky, holy light, light from clouds, fantasy theme, expressiveh, d4rk01l",
+            //"score_9, score_8_up, score_8, score_7_up, score_7, score_6_up, score_6, score_5_up, Score_5, girl, beautiful face, turn back, wet small hair,  narrow hips , naked, juicy ass, sexy adult  , posing , halfbody portrait , back view , bathroom , wet , soapy body , foam , shower , from below , luxury bathroom , blue tiles , dynamic",
+            "score_9, score_8_up, score_8_up, 1girl, in a bathtub, naked, soap,  ((sexy eye contact)), parted lips, kneeling, Ass up,  nsfw,  ((licking cock)), wet skin, showing off her body,  dreamy eyes, sweaty skin, panam palmer, arching back,  fit body, thick thighs,, thin waists, godrays, shower tiles, bathroom interior, steam, rain, view from above,  chairs, g0thicPXL, GTA,",
+            //"score_6, score_5, score_4, pony, gaping, muscular, censored, furry, milf, child, kid, chibi, monochrome, grayscale, bra, panties, upturned eyes"
+            //"score_6, score_5, score_4, pony, muscular, censored, furry"
+            //"core_6, score_5, score_4, worst quality, low quality, text, censored, deformed, bad hand, blurry, (watermark), multiple phones, weights, bunny ears, extra hands,"
+            "core_6, score_5, score_4, worst quality, low quality, text, censored, deformed, bad hand, blurry, (watermark), multiple phones, weights, bunny ears, extra hands,"
             );
-
         Console.WriteLine("prompt created");
 
         PyObject generateImage = module.GetAttr("generateImage");
@@ -55,9 +61,9 @@ internal class Program
             -1,
             816,           // width
             1152,           // height
-            50,
-            7.5,
-            1);
+            40,
+            7,
+            2);
         for (int i = 0; i < 10; ++i)
         {
             GenerateImage(generateImage, p);
@@ -87,14 +93,14 @@ print(s)
 
     private static void SaveImage(PyObject bytes)
     {
-        PyBytes result = new (bytes);
+        PyBytes result = new(bytes);
 
         byte[] data = result.ToArray();
         Span<byte> chunk = stackalloc byte[1024];
         int length = result.Size;
 
         string path = "C:\\Projects\\2024\\PythonInterop\\images";
-        if(!Directory.Exists(path))
+        if (!Directory.Exists(path))
         {
             Directory.CreateDirectory(path);
         }
--- src/Examples/PythonSample/example.py
+++ src/Examples/PythonSample/example.py
@@ -34,11 +34,32 @@ def createPipeline(modelPath: str):
         torch_dtype=torch.bfloat16, 
         variant="fp16", 
         use_safetensors=True).to("cuda")
+
+    pipeline.load_lora_weights("C:\StableDiffusion\models\loras\Fant5yP0ny.safetensors", adapter_name = "Fant5yP0ny")
+    pipeline.load_lora_weights("C:\StableDiffusion\models\loras\Expressive_H-000001.safetensors", adapter_name = "Expressive_H")
+    pipeline.load_lora_weights("C:\StableDiffusion\models\loras\RELSM_v1.safetensors", "relsm")
+    pipeline.load_lora_weights("C:\StableDiffusion\models\loras\d4rk01lXLP.safetensors", "d4rk01l")
+    pipeline.load_lora_weights("C:\StableDiffusion\models\loras\Concept Art Twilight Style SDXL_LoRA_Pony Diffusion V6 XL.safetensors", "twilight")
+    scales = {
+        "text_encoder": 0.7,
+        "text_encoder_2": 0.7,  # only usable if pipe has a 2nd text encoder
+        "unet": {
+            "down": 0.9,  # all transformers in the down-part will use scale 0.9
+            # "mid"  # in this example "mid" is not given, therefore all transformers in the mid part will use the default scale 1.0
+            "up": {
+                "block_0": 0.6,  # all 3 transformers in the 0th block in the up-part will use scale 0.6
+                "block_1": [0.4, 0.8, 1.0],  # the 3 transformers in the 1st block in the up-part will use scales 0.4, 0.8 and 1.0 respectively
+            }
+        }
+    }
+    pipeline.set_adapters(["Fant5yP0ny", "Expressive_H", "relsm", "d4rk01l", "twilight"], [scales, 0.6, 0.9, 0.8, 0.8])    
+
     scheduler = EulerAncestralDiscreteScheduler.from_config(pipeline.scheduler.config)
     scheduler.config.use_karras_sigmas = False
     # scheduler.config.euler_at_final = True    
     # scheduler.config.algorithm_type = "sde-dpmsolver++"
     pipeline.scheduler = scheduler
+    
     pipeline.enable_xformers_memory_efficient_attention()
     pipeline.enable_model_cpu_offload()
     return pipeline    
